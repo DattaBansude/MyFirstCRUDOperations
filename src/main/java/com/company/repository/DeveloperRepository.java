d1a175bb9ff36ba514f360ac8768be2015b3dc71
“DattaBansude” <dattatraybansude91@gmail.com>
2025-08-24 10:25:52 +0530

    add schedulers for find missdeveloperId and update it

--- src/main/java/com/company/repository/DeveloperRepository.java
+++ src/main/java/com/company/repository/DeveloperRepository.java
@@ -1,7 +1,9 @@
 package com.company.repository;
 
 import com.company.entity.Developer;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -16,4 +18,12 @@ public interface DeveloperRepository extends JpaRepository<Developer,Integer> {
 
     @Query(value = "SELECT * FROM Developer",nativeQuery = true)
     List<Developer> findAllDeveloper();
+
+    @Query(value = "SELECT * FROM Developer d where d.developer_id is NULL OR d.developer_id=' '",nativeQuery = true)
+    List<Developer> findmissDeveloperId();
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE Developer SET developer_id=:newdevId where id=:id",nativeQuery = true)
+    int updateDeveloperId(int id, String newdevId);
 }
